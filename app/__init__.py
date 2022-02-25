7d6b41d2e4f0b8c41febc3ba16dc20abb3710ae6
hsolis <hsolis11@outlook.com>
2022-02-25 10:03:36 -0600

    adding login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import settings
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = settings.APP_SECRET_KEY
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from app import routes
